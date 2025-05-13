2d9b0cc32df72da17c7a485b3792ff03ce6b995a
SamuelSilva-DEVL <samuelmec1@gmail.com>
2025-05-12 22:41:05 -0300

    style: - ajuste imagem hero - ajuste texto seção recrutadores

--- src/app/_components/contacts.tsx
+++ src/app/_components/contacts.tsx
@@ -2,7 +2,7 @@ export default function Contacts() {
   return (
     <section className="w-full h-[1000px] md:h-[550px] lg:h-[800px] 2xl:h-[950px] relative overflow-hidden">
       <div className="container mx-auto px-4 py-2 md:py-12 z-10 absolute left-[50%] top-[80px] md:top-[100px] translate-x-[-50%] translate-y-[-50%]">
-        <h2 className="w-full md:w-4/5 m-auto text-[20px] lg:text-4xl font-bold text-center mb-16 text-[var(--text-primary-color-sn)]">
+        <h2 className="w-full md:w-4/5 m-auto text-[16px] lg:text-4xl font-bold text-center mb-16 text-[var(--text-primary-color-sn)]">
           Mercado superaquecido com oportunidades nacionais e internacionais
         </h2>
       </div>
