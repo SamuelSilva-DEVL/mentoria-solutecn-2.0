c0a92118a6b5d6e9f40e6f57d4dedd8b8c533559
SamuelSilva-DEVL <samuelmec1@gmail.com>
2025-05-15 09:57:39 -0300

    fix: responsividade texto seção recrutadores

--- src/app/_components/contacts.tsx
+++ src/app/_components/contacts.tsx
@@ -69,9 +69,9 @@ export default function Contacts() {
     }, [emblaApi]);
     
   return (
-    <section className="w-full h-full md:h-[550px] lg:h-[800px] 2xl:h-[950px] relative overflow-hidden bg-[#0f0f0f] md:bg-[#050D22] text-[var(--text-primary-color-sn)] py-4">
-      <div className="hidden md:block container mx-auto px-4 py-2 md:py-12 z-10 absolute left-[50%] top-[80px] md:top-[100px] translate-x-[-50%] translate-y-[-50%]">
-        <h2 className="w-full md:w-4/5 m-auto text-[16px] lg:text-4xl font-bold text-center mb-16">
+    <section className="w-full h-full md:h-[550px] lg:h-[800px] 2xl:h-[950px] relative overflow-hidden bg-[#0f0f0f] md:bg-[#000000] text-[var(--text-primary-color-sn)] py-4">
+      <div className="hidden md:block container mx-auto px-4 py-2 z-10 absolute left-[50%] top-[80px] md:top-[80px] lg:top-[100px] translate-x-[-50%] translate-y-[-50%]">
+        <h2 className="w-2/3 m-auto text-[16px] lg:text-4xl font-bold text-center mb-16">
           Mercado superaquecido com oportunidades nacionais e internacionais
         </h2>
       </div>
